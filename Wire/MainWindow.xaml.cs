b556bc2a8ba7017307070323d8fcd6773087fc0c
Jure Runje <jurerunje@gmail.com>
2014-09-07 21:07:10 +0200

    - rework

--- Wire/MainWindow.xaml.cs
+++ Wire/MainWindow.xaml.cs
@@ -117,28 +117,5 @@ namespace Wire
         const int MaxBrojZicaUSnopu = 11;
         double[,] Matrica = new double[Zice.Count, MaxBrojZicaUSnopu];
     }
-
-
-    public class ResultToBackgroundConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            if (value is int)
-            {
-                int quantity = (int)value;
-
-
-                    byte n = (byte)((255 / Math.Pow(quantity + 1,0.2)));
-
-                    return new SolidColorBrush(Color.FromRgb(n,n,n));
-            }
-
-            return Brushes.White;
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            throw new NotImplementedException();
-        }
-    }  
+ 
 }
---
+++ Wire/ResultToBackgroundConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Wire
+{
+    public class ResultToBackgroundConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is int)
+            {
+                int quantity = (int)value;
+
+                byte n = (byte)((255 / Math.Pow(quantity + 1, 0.2)));
+
+                return new SolidColorBrush(Color.FromRgb(n, n, n));
+            }
+
+            return Brushes.White;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
