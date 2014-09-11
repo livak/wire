595e39165ce814915a35c05262cfc1fa618800cd
Jure Runje <jurerunje@gmail.com>
2014-09-11 23:39:08 +0200

    - ako se slučajno doda žica krivim redoslijedom

--- Wire/Configuration.cs
+++ Wire/Configuration.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace Wire
 {
@@ -10,6 +11,6 @@ namespace Wire
             .315,   .335,   .355,   .375,   .4,     .425,   .45,    .475, 
             .5,     .56,    .6,     .63,    .65,    .67,    .71,    .75,    .8, 
             .85,    .9,     .95,    1,      1.06,   1.1,    1.12,   1.25,   1.5,   1.6,   1.7
-        };
+        }.OrderBy(x => x).ToList();
     }
 }
